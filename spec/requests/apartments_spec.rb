28a971887ab5573f5ce82e71532e6b18e1727ec3
XavierB91 <xavierbarker183@gmail.com>
2023-04-13 10:37:03 -0700

    backend-index

--- spec/requests/apartments_spec.rb
+++ spec/requests/apartments_spec.rb
@@ -1,7 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe "Apartments", type: :request do
+  let (:user) {User.create email: "test@testing.com", password: "test123", password_confirmation: "test123"}
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "gets a list of apartments" do 
+    user.apartments.create(
+            street: '123 Fourth St',
+            city: 'San Diego',
+            state: 'CA',
+            manager: 'Den Apartments',
+            email: 'apartments@email.com',
+            price: '1500000',
+            bedrooms: 4,
+            bathrooms: 4,
+            pets: 'yes',
+            image: 'https://images.squarespace-cdn.com/content/v1/5c47817996e76f8fca739d4d/1548982975169-Y4J3Z5XQ7ZPHVEZ1P90J/little-italy-san-diego-apartments-493.016.jpg'
+    )
+      get "/apartments"
+      apartment= JSON.parse(response.body)
+      expect(response).to have_http_status(200)
+      expect(apartment.length).to eq 1
+    end
   end
 end
