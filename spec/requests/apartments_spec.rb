66fe396e5a42e82f36225ab0426014893a7950c1
csaument <c.s.aument@gmail.com>
2023-04-13 10:39:39 -0700

    completes testing validation, completes b:index card

--- spec/requests/apartments_spec.rb
+++ spec/requests/apartments_spec.rb
@@ -4,18 +4,18 @@ RSpec.describe "Apartments", type: :request do
   let (:user) {User.create email: "test@testing.com", password: "test123", password_confirmation: "test123"}
   describe "GET /index" do
     it "gets a list of apartments" do 
-    user.apartments.create(
-            street: '123 Fourth St',
-            city: 'San Diego',
-            state: 'CA',
-            manager: 'Den Apartments',
-            email: 'apartments@email.com',
-            price: '1500000',
-            bedrooms: 4,
-            bathrooms: 4,
-            pets: 'yes',
-            image: 'https://images.squarespace-cdn.com/content/v1/5c47817996e76f8fca739d4d/1548982975169-Y4J3Z5XQ7ZPHVEZ1P90J/little-italy-san-diego-apartments-493.016.jpg'
-    )
+      user.apartments.create(
+        street: '123 Fourth St',
+        city: 'San Diego',
+        state: 'CA',
+        manager: 'Den Apartments',
+        email: 'apartments@email.com',
+        price: '1500000',
+        bedrooms: 4,
+        bathrooms: 4,
+        pets: 'yes',
+        image: 'https://images.squarespace-cdn.com/content/v1/5c47817996e76f8fca739d4d/1548982975169-Y4J3Z5XQ7ZPHVEZ1P90J/little-italy-san-diego-apartments-493.016.jpg'
+      )
       get "/apartments"
       apartment= JSON.parse(response.body)
       expect(response).to have_http_status(200)
